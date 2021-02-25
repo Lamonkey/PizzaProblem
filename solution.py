d0e0e277cbd46494ab7514280a2834d2224c6025
lamonkey <88therisingsun@gmail.com>
2021-02-25 05:23:13 -0500

    added parallel in selectTeam

--- solution.py
+++ solution.py
@@ -1,3 +1,4 @@
+import multiprocessing as mp
 #read input file
 def readInput(file):
     numPizza = 0
@@ -37,7 +38,7 @@ def selectTeam(numPizza,ingreds,teams,pizzas):
     pizzas = sorted(pizzas, key=lambda x:x[0],reverse=True)
     i = len(teams)-1
     #i = 0
-   
+    result = mp.Queue()
     while (teams[0] >0 or teams[1] > 0 or teams[2] > 0):
         if(len(pizzas) == 0):
             break
@@ -46,12 +47,38 @@ def selectTeam(numPizza,ingreds,teams,pizzas):
                 if(len(pizzas) < 2 and teams[0]>0):
                     break
         perfectSocre1,perfectSocre2,perfectSocre3=-1,-1,-1
+        pool = []
+        p1Flag,p2Flag,p3Flag = False,False,False
         if(teams[0] >0 and 2 <= len(pizzas)):
-            tmpReuslt1,pizzas1,perfectSocre1 = calculateScore(2,ingreds,pizzas)
+            p1Flag = True
+            p1 = mp.Process(target=calculateScore(2,ingreds,pizzas,result))
+            p1.start()
+            pool.append(p1)
+            #tmpReuslt1,pizzas1,perfectSocre1 = calculateScore(2,ingreds,pizzas)
         if(teams[1] >0 and 3 <= len(pizzas)):
-            tmpReuslt2,pizzas2,perfectSocre2 = calculateScore(3,ingreds,pizzas)
+            p2Flag = True
+            p2 = mp.Process(target=calculateScore(3,ingreds,pizzas,result))
+            p2.start()
+            pool.append(p2)
+            #tmpReuslt2,pizzas2,perfectSocre2 = calculateScore(3,ingreds,pizzas)
         if(teams[2] >0 and 4 <= len(pizzas)):
-            tmpReuslt3,pizzas3,perfectSocre3 = calculateScore(4,ingreds,pizzas)
+            p3Flag = True
+            p3 = mp.Process(target=calculateScore(4,ingreds,pizzas,result))
+            p3.start()
+            pool.append(p3)
+            #tmpReuslt3,pizzas3,perfectSocre3 = calculateScore(4,ingreds,pizzas)
+        for p in pool:
+            p.join()
+        if(p1Flag):
+            result1 = result.get()
+            tmpReuslt1,pizzas1,perfectSocre1 = result1["ret"],result1["dupPizza"],result1["perfectScore"]
+        if(p2Flag):
+            result2 = result.get()
+            tmpReuslt2,pizzas2,perfectSocre2 = result2["ret"],result2["dupPizza"],result2["perfectScore"]
+        if(p3Flag):
+            result3 = result.get()
+            tmpReuslt3,pizzas3,perfectSocre3 = result3["ret"],result3["dupPizza"],result3["perfectScore"]
+
         if(perfectSocre1 >= perfectSocre2 and perfectSocre1 >= perfectSocre3 and perfectSocre1 >= 0 ):
             retTeam.append(tmpReuslt1)
             teams[0] = teams[0] - 1
@@ -69,7 +96,8 @@ def selectTeam(numPizza,ingreds,teams,pizzas):
 #select pizza with most ingreds first, then find next one with most 
 #ingreds while max the score
 #calculate the best combination for numberOfPeople pizza
-def calculateScore(numberOfPeople, ingreds, pizzas):
+def calculateScore(numberOfPeople, ingreds, pizzas,result):
+    outDict = {}
     ret = []
     perfectSore = 0
     ingredsCopy = ingreds.copy()
@@ -92,7 +120,11 @@ def calculateScore(numberOfPeople, ingreds, pizzas):
                 dupPizza.remove(pizzas[pizza])
                 selectedPizza = selectedPizza + 1
         pizzas = dupPizza.copy()
-    return ret,dupPizza,perfectSore
+    outDict["ret"] = ret
+    outDict["dupPizza"] = dupPizza
+    outDict["perfectScore"] = perfectSore
+    result.put(outDict)
+    #return ret,dupPizza,perfectSore
 
 #this function use to genrate submission
 def generateSub(file,numPizza,ingreds,teams,pizzas):
@@ -106,13 +138,14 @@ def generateSub(file,numPizza,ingreds,teams,pizzas):
         f.write("\n")
     f.close()
 
-numPizza,ingreds,teams,pizzas = readInput("a_example")
-generateSub("a.txt",numPizza,ingreds,teams,pizzas)
-numPizza,ingreds,teams,pizzas = readInput("b_little_bit_of_everything.in")
-generateSub("b.txt",numPizza,ingreds,teams,pizzas)
-# numPizza,ingreds,teams,pizzas = readInput("c_many_ingredients.in")
-# generateSub("c.txt",numPizza,ingreds,teams,pizzas)
-# numPizza,ingreds,teams,pizzas = readInput("d_many_pizzas.in")
-# generateSub("d.txt",numPizza,ingreds,teams,pizzas)
-# numPizza,ingreds,teams,pizzas = readInput("e_many_teams.in")
-# generateSub("e.txt",numPizza,ingreds,teams,pizzas)
+if __name__ == '__main__':    
+    numPizza,ingreds,teams,pizzas = readInput("a_example")
+    generateSub("a.txt",numPizza,ingreds,teams,pizzas)
+    numPizza,ingreds,teams,pizzas = readInput("b_little_bit_of_everything.in")
+    generateSub("b.txt",numPizza,ingreds,teams,pizzas)
+    numPizza,ingreds,teams,pizzas = readInput("c_many_ingredients.in")
+    generateSub("c.txt",numPizza,ingreds,teams,pizzas)
+    numPizza,ingreds,teams,pizzas = readInput("d_many_pizzas.in")
+    generateSub("d.txt",numPizza,ingreds,teams,pizzas)
+    numPizza,ingreds,teams,pizzas = readInput("e_many_teams.in")
+    generateSub("e.txt",numPizza,ingreds,teams,pizzas)
