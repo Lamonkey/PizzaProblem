adbd767ec9d9e785f393f11aba4bbb4c7235903d
lamonkey <88therisingsun@gmail.com>
2021-02-25 06:41:35 -0500

    use parallel to execute file to increase performance

--- solution.py
+++ solution.py
@@ -1,4 +1,5 @@
 #read input file
+import sys
 def readInput(file):
     numPizza = 0
     ingreds = {}
@@ -38,9 +39,9 @@ def selectTeam(file,output):
     pizzas = sorted(pizzas, key=lambda x:x[0],reverse=True)
     i = len(teams)-1
     #i = 0
-   
+    totalSum = 0
     while (teams[0] >0 or teams[1] > 0 or teams[2] > 0):
-        if(len(pizzas) == 0):
+        if(len(pizzas) <= 1):
             break
         if(len(pizzas)<4 and teams[2] > 0):
             if(len(pizzas)< 3 and teams[1] > 0):
@@ -48,29 +49,35 @@ def selectTeam(file,output):
                     break
         perfectSocre1,perfectSocre2,perfectSocre3=-1,-1,-1
         if(teams[0] >0 and 2 <= len(pizzas)):
-            tmpReuslt1,pizzas1,perfectSocre1 = calculateScore(2,ingreds,pizzas)
+            tmpReuslt1,pizzas1,perfectSocre1,sum1 = calculateScore(2,ingreds,pizzas)
         if(teams[1] >0 and 3 <= len(pizzas)):
-            tmpReuslt2,pizzas2,perfectSocre2 = calculateScore(3,ingreds,pizzas)
+            tmpReuslt2,pizzas2,perfectSocre2,sum2 = calculateScore(3,ingreds,pizzas)
         if(teams[2] >0 and 4 <= len(pizzas)):
-            tmpReuslt3,pizzas3,perfectSocre3 = calculateScore(4,ingreds,pizzas)
-        if(perfectSocre1 >= perfectSocre2 and perfectSocre1 >= perfectSocre3 and perfectSocre1 >= 0 ):
-            retTeam.append(tmpReuslt1)
-            teams[0] = teams[0] - 1
-            pizzas = pizzas1
+            tmpReuslt3,pizzas3,perfectSocre3,sum3 = calculateScore(4,ingreds,pizzas)
+        if(perfectSocre3 >= perfectSocre2 and perfectSocre3 >= perfectSocre1 and perfectSocre3 >= 0 ):
+            retTeam.append(tmpReuslt3)
+            teams[2] = teams[2] - 1
+            pizzas = pizzas3
+            totalSum = totalSum + sum3 * sum3
         elif(perfectSocre2 >= perfectSocre1 and perfectSocre2 >= perfectSocre3 and perfectSocre2 >= 0):
             retTeam.append(tmpReuslt2)
             teams[1] = teams[1] - 1
             pizzas = pizzas2
-        elif(perfectSocre3 >= 0):
-            retTeam.append(tmpReuslt3)
-            teams[2] = teams[2] - 1
-            pizzas = pizzas3
+            totalSum = totalSum + sum2 * sum2
+        elif(perfectSocre1 >= 0):
+            retTeam.append(tmpReuslt1)
+            teams[0] = teams[0] - 1
+            pizzas = pizzas1
+            totalSum = totalSum + sum3 * sum3
+    print(totalSum)
+    sys.stdout.flush()
     generateSub(output,retTeam)
 
 #select pizza with most ingreds first, then find next one with most 
 #ingreds while max the score
 #calculate the best combination for numberOfPeople pizza
 def calculateScore(numberOfPeople, ingreds, pizzas):
+    total = 0
     ret = []
     perfectSore = 0
     ingredsCopy = ingreds.copy()
@@ -89,11 +96,12 @@ def calculateScore(numberOfPeople, ingreds, pizzas):
                     perfectSore = perfectSore + 1
                 for ing in pizzas[pizza][2]:
                     ingredsCopy[ing] = False
+                total = total + sum
                 ret.append(pizzas[pizza][1])
                 dupPizza.remove(pizzas[pizza])
                 selectedPizza = selectedPizza + 1
         pizzas = dupPizza.copy()
-    return ret,dupPizza,perfectSore
+    return ret,dupPizza,perfectSore,total
 
 #this function use to genrate submission
 def generateSub(file,result):
@@ -108,20 +116,21 @@ def generateSub(file,result):
 import multiprocessing as mp
 if __name__ == "__main__":
     pool = []
-    p1 = mp.Process(target=selectTeam("a_example","a.txt"))
+    p1 = mp.Process(target=selectTeam,args=("a_example","a.txt"))
     p1.start()
     pool.append(p1)
-    p2 = mp.Process(target=selectTeam("b_little_bit_of_everything.in","b.txt"))
+    p2 = mp.Process(target=selectTeam,args=("b_little_bit_of_everything.in","b.txt"))
     p2.start()
     pool.append(p2)
-    p3 = mp.Process(target=selectTeam("c_many_ingredients.in","c.txt"))
+    p3 = mp.Process(target=selectTeam,args=("c_many_ingredients.in","c.txt"))
     p3.start()
     pool.append(p3)
-    p4 = mp.Process(target=selectTeam("d_many_pizzas.in","d.txt"))
+    p4 = mp.Process(target=selectTeam,args=("d_many_pizzas.in","d.txt"))
     p4.start()
     pool.append(p4)
-    p5 = mp.Process(target=selectTeam("e_many_teams.in","e.txt"))
+    p5 = mp.Process(target=selectTeam,args=("e_many_teams.in","e.txt"))
     p5.start()
     pool.append(p5)
     for p in pool:
         p.join()
+#selectTeam("b_little_bit_of_everything.in","b.txt")
