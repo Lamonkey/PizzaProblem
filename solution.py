a8fda88799cf3da6f022ba4bd961c0d7bb5ed8ba
lamonkey <88therisingsun@gmail.com>
2021-02-24 21:41:16 -0500

    add frame

--- solution.py
+++ solution.py
@@ -8,10 +8,12 @@ def readInput(file):
     return numPizza,ingreds,teams,pizzas
 #ingred {"onion":true,mushroom:true....}
 #pizzas[[onion,pepepr,olive],[mushroom,tomato,basil]]
+#1.select team with most person first
 def selectTeam(numPizza,ingreds,teams,pizzas):
     #return [[teamType,pizza1,pizza2],[teamType2,pizza3,pizza4]]
     return []
-
+#select pizza with most ingreds first, then find next one with most 
+#ingreds while max the score
 #calculate the best combination for numberOfPeople pizza
 def calculateScore(numberOfPeople,ingreds):
     #return [teamType,pizza1,pizza2]
