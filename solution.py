9ad1afaaf34d24a3930af525d7489ed172be9466
lamonkey <88therisingsun@gmail.com>
2021-02-24 21:24:12 -0500

    push structure

---
+++ solution.py
@@ -0,0 +1,22 @@
+#read input file
+def readInput(file):
+    numPizza = 0
+    ingreds = {}
+    pizzas = []
+    teams = []
+
+    return numPizza,ingreds,teams,pizzas
+#ingred {"onion":true,mushroom:true....}
+#pizzas[[onion,pepepr,olive],[mushroom,tomato,basil]]
+def selectTeam(numPizza,ingreds,teams,pizzas):
+    #return [[teamType,pizza1,pizza2],[teamType2,pizza3,pizza4]]
+    return []
+
+#calculate the best combination for numberOfPeople pizza
+def calculateScore(numberOfPeople,ingreds):
+    #return [teamType,pizza1,pizza2]
+    return []
+
+#this function use to genrate submission
+def generateSub():
+    pass
